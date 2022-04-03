e95ff6e777bfb2addb09383dfd9d52a0acb9086b
FahamidulDipro <mdfahamidulislam047@gmail.com>
2022-04-04 02:03:15 +0600

    customer review section in home page created

--- src/components/Home/Home.js
+++ src/components/Home/Home.js
@@ -1,13 +1,34 @@
-import React from 'react';
- 
-
+import { Button } from "bootstrap";
+import React from "react";
+import { Col, Container, Row } from "react-bootstrap";
+import trimmer from "../../images/trimmer.jpg";
+import "./Home.css";
 const Home = () => {
-    return (
-        <div>
-            <h1>This is Homepage</h1>
-            
-        </div>
-    );
+  return (
+    <div className="mt-5 container">
+      <Container fluid>
+        <Row>
+          <Col md={6} sm={12}>
+            <h1 className="text-start p-3">
+              The Best Beard Trimmer
+              <br />
+              <span className="colored-heading">
+                Ultra sharp with Awesome Build Quality
+              </span>
+            </h1>
+          </Col>
+          <Col md={6} sm={12}>
+            <img src={trimmer} alt="trimmer" className="rounded" />
+          </Col>
+        </Row>
+      </Container>
+
+      <Container fluid className="customer-review">
+        <h1>Customer Reviews</h1>
+        <button className="btn btn-primary mt-5">See All Reviews</button>
+      </Container>
+    </div>
+  );
 };
 
 export default Home;
