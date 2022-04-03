b3a3ee301bba266f9593bb75c57cd560e7fe36ed
FahamidulDipro <mdfahamidulislam047@gmail.com>
2022-04-04 01:41:15 +0600

    Routers built for navigation

--- src/App.js
+++ src/App.js
@@ -1,13 +1,32 @@
 import logo from './logo.svg';
 import './App.css';
-import { Button, Card } from 'react-bootstrap';
+import { Button, Card} from 'react-bootstrap';
+import Navigation from './components/Navigation/Navigation';
+import Home from './components/Home/Home';
+import 'bootstrap/dist/css/bootstrap.min.css';
+import { Route, Routes } from 'react-router-dom';
+import About from './components/About/About';
+import Dashboard from './components/Dashboard/Dashboard';
+import Blogs from './components/Blogs/Blogs';
+import Reviews from './components/Reviews/Reviews';
+ 
+ 
+ 
+
 
 function App() {
   return (
     <div className="App">
-     <h1>Welcome to trimmer review</h1>
-     <Button>Click</Button>
-     
+ 
+     <Navigation></Navigation>
+     <Routes>
+       <Route path='/' element={<Home></Home>}></Route>
+       <Route path='/about' element={<About></About>}></Route>
+       <Route path='/dashboard' element={<Dashboard></Dashboard>}></Route>
+       <Route path='/blogs' element={<Blogs></Blogs>}></Route>
+       <Route path='/reviews' element={<Reviews></Reviews>}></Route>
+     </Routes>
+
     </div>
   );
 }
---
+++ src/components/Navigation/Navigation.js
@@ -0,0 +1,28 @@
+import React from 'react';
+import { Container, Nav, Navbar } from 'react-bootstrap';
+import { NavLink } from 'react-router-dom';
+import './Navigation.css';
+const Navigation = () => {
+    return (
+         
+            <Navbar bg="light" expand="lg">
+  <Container>
+    <Navbar.Brand href="/">Trimmer Review</Navbar.Brand>
+    <Navbar.Toggle aria-controls="basic-navbar-nav" />
+    <Navbar.Collapse id="basic-navbar-nav">
+      <Nav className="me-auto">
+        <NavLink to="/" className={'mx-2 text-dark text-decoration-none'}>Home</NavLink>
+        <NavLink to="/reviews" className={'mx-2 text-dark text-decoration-none'}>Reviews</NavLink>
+        <NavLink to="/dashboard" className={'mx-2 text-dark text-decoration-none'}>Dashboard</NavLink>
+        <NavLink to="/blogs" className={'mx-2 text-dark text-decoration-none'}>Blogs</NavLink>
+        <NavLink to="/about" className={'mx-2 text-dark text-decoration-none'}>About</NavLink>
+       
+      </Nav>
+    </Navbar.Collapse>
+  </Container>
+</Navbar>
+       
+    );
+};
+
+export default Navigation;
