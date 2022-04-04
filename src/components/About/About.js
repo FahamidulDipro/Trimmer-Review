861c36b9394a653a3ee4599962296328ebd91830
FahamidulDipro <mdfahamidulislam047@gmail.com>
2022-04-05 01:59:18 +0600

    About page added

--- src/components/About/About.js
+++ src/components/About/About.js
@@ -1,11 +1,22 @@
-import React from 'react';
+import React from "react";
 
 const About = () => {
-    return (
-        <div style={{marginTop:'200px'}} className='container'>
-            <h1 className='text-start'>About Us</h1>
-        </div>
-    );
+  return (
+    <div
+      style={{ marginTop: "200px" }}
+      className="container text-start p-5 shadow-lg"
+    >
+      <h1 style={{ color: "peru" }}>About Us</h1>
+      <hr />
+      <p style={{ fontSize: "20px" }}>
+        <b>Trimmer Review</b> is a SPA(Single Page Application) built with
+        React. The app contains user reviews and ratings of a trimmer. The app
+        also contains a dashboard where some data are shown in different charts.
+        The app contains a blog page. React router and Context API were used to
+        build this SPA.
+      </p>
+    </div>
+  );
 };
 
 export default About;
