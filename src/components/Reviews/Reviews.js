0f6d750d0107b55d3428d09ae791e845cc83d999
FahamidulDipro <mdfahamidulislam047@gmail.com>
2022-04-05 02:15:18 +0600

    favicon changed

--- src/components/Reviews/Reviews.js
+++ src/components/Reviews/Reviews.js
@@ -1,5 +1,3 @@
-import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
-import { faStar, faStarHalf } from "@fortawesome/free-solid-svg-icons";
 import React from "react";
 import { Card, Col } from "react-bootstrap";
 
